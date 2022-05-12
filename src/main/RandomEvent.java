907b1747b1cc85048bf6d4a0be5af86aae733310
Mustapha Conteh <mco176@uclive.ac.nz>
2022-05-12 22:48:27 +1200

    added comments to  RandomEvent

--- src/main/RandomEvent.java
+++ src/main/RandomEvent.java
@@ -1,8 +1,8 @@
 package main;
 
-
+/**An abstract class that has subclasses that are random events and all must implement the method startEvent**/
 public abstract class RandomEvent {
-	/**An abstract class that has subclasses that are random events and all must implement the method startEvent**/
+	/**An abstract method which all subclasses of RandomEvent must implement their different implementations is what creates "Random  Events", takes a Monster variable mon, and a Player variable player as its parameters, returns void**/
 	abstract public void startEvent(Monster mon, Player player);
 
 	
